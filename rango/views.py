b8798e149f1fc778b97a1754765149b0520d0dfd
Domenico <2201547R@student.gla.ac.uk>
2017-02-03 17:16:24 +0000

    4.1

--- rango/views.py
+++ rango/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-	return HttpResponse("Rango says hey there partner! <br/> <a href='/rango/about/'>About</a>")
+    context_dict = {'boldmessage': "Crunchy, creamy, cookie, cangy, cupcake!"}
 
+    return render(request, 'rango/index.html', context=context_dict)
+
+    ##return HttpResponse("Rango says hey there partner! <br/> <a href='/rango/about/'>About</a>")
 def about(request):
-	return HttpResponse("Rango says this is the about Page <br/> Created by Domenico Restuccia UofG 2201547 <br/> <a href='/rango/index/'>Index</a>")
+    return HttpResponse("Rango says this is the about Page <br/> Created by Domenico Restuccia UofG 2201547 <br/> <a href='/rango/index/'>Index</a>")
 
