12773477ace4bacee520c41edcb6893a80c0dcc9
Domenico <2201547R@student.gla.ac.uk>
2017-02-03 17:33:06 +0000

    4.3

--- Tango_excercises/settings.py
+++ Tango_excercises/settings.py
@@ -16,6 +16,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 
 # Quick-start development settings - unsuitable for production
@@ -55,7 +56,8 @@ MIDDLEWARE_CLASSES = [
 
 ROOT_URLCONF = 'Tango_excercises.urls'
 
-
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
 
 
 TEMPLATES = [
@@ -69,6 +71,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
