f4a6ac18d6d86a0769829a702d2e9fa6f2cf72c0
Domenico <2201547R@student.gla.ac.uk>
2017-02-03 16:59:46 +0000

    Chapter 3 Completed

--- rango/urls.py
+++ rango/urls.py
@@ -2,5 +2,11 @@ from django.conf.urls import url
 from rango import views
 
 urlpatterns = [
+
+    url(r'^about/', views.about, name='about'),
+    url(r'^index/', views.index, name='index'),
     url(r'^$', views.index, name='index'),
+
+
+
 ]
--- rango/views.py
+++ rango/views.py
@@ -3,4 +3,8 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-	return HttpResponse("Rango says hey there partner!")
+	return HttpResponse("Rango says hey there partner! <br/> <a href='/rango/about/'>About</a>")
+
+def about(request):
+	return HttpResponse("Rango says this is the about Page <br/> Created by Domenico Restuccia UofG 2201547 <br/> <a href='/rango/index/'>Index</a>")
+
