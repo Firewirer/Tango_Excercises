3947f695dc00bdd48b5746d7fc5f5567dbf3a83b
Domenico <2201547R@student.gla.ac.uk>
2017-02-03 17:40:19 +0000

    Completed Chapter 4

--- rango/views.py
+++ rango/views.py
@@ -10,5 +10,9 @@ def index(request):
 
     ##return HttpResponse("Rango says hey there partner! <br/> <a href='/rango/about/'>About</a>")
 def about(request):
-    return HttpResponse("Rango says this is the about Page <br/> Created by Domenico Restuccia UofG 2201547 <br/> <a href='/rango/index/'>Index</a>")
+    context_dict = {'aboutmessage': "Created by Domenico Restuccia UofG 2201547",}
+
+    return render(request, 'rango/about.html', context=context_dict)
+
+    ##return HttpResponse("Rango says this is the about Page <br/> Created by Domenico Restuccia UofG 2201547 <br/> <a href='/rango/index/'>Index</a>")
 
