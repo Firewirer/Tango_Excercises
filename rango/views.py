8840f14080f358f7e00dec3088b78907c44060c1
Domenico <2201547R@student.gla.ac.uk>
2017-02-09 20:15:08 +0000

    end chapter 6

--- rango/views.py
+++ rango/views.py
@@ -2,13 +2,45 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
 from rango.models import Category
+from rango.models import Page
 # Create your views here.
+
+def show_category(request, category_name_slug):
+
+    context_dict = {}
+
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+
+        pages = Page.objects.filter(category=category)
+
+        context_dict['pages'] = pages
+
+        context_dict['category'] = category
+
+    except Category.DoesNotExist:
+
+        context_dict['category'] = None
+        context_dict['pages'] = None
+
+    return render(request, 'rango/category.html', context_dict)
+
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
+    pages_list = Page.objects.order_by('-views')[:5]
     context_dict = {
         'categories': category_list,
+        'pages': pages_list,
     }
+    ##topfivelist = []
+
+    ##for pages in Page.objects.all():
+    ##    for i in range(0,5):
+    ##        if pages.topfivelist[i]
+
+
+
 
     return render(request, 'rango/index.html', context=context_dict)
 
